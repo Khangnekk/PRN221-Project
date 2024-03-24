815e89645df24363622a75ed5223fc8d1a10b4ee
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2024-03-25 01:02:27 +0700

    Update Program.cs

--- ProjectPRN221/ProjectPRN221/Program.cs
+++ ProjectPRN221/ProjectPRN221/Program.cs
@@ -1,5 +1,10 @@
+using Business_Object.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<ProjectPRN221Context>(
+	opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("MyConnectionStrings")));
 var app = builder.Build();
 app.UseStaticFiles();
 app.MapRazorPages();
