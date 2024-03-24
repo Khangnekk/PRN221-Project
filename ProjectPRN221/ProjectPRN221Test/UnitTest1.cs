f8fb0d276319a3f3f32379a91c6f2c492181207a
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2024-03-24 20:17:52 +0700

    Project test (test session handler)

---
+++ ProjectPRN221/ProjectPRN221Test/UnitTest1.cs
@@ -0,0 +1,130 @@
+using Data_Access.DTOs;
+using ProjectPRN221.Helper;
+
+namespace ProjectPRN221Test
+{
+	public class Tests
+	{
+
+		[Test]
+		public void ConvertToSessionDTOCreates_TestCountSessionCreate()
+		{
+			SessionDTORaw rawSession = new SessionDTORaw
+			{
+				SubjectId = "PRN211",
+				GroupId = "SE1643",
+				RoomRaw = "BE-102",
+				FirstDate = new DateTime(2024, 3, 24),
+				TimeslotRaw = "A24",
+				LecturerId = "chilp",
+				TotalSession = 20,
+				Online = true
+			};
+
+			var sessionDTOCreates = SessionHelper.ConvertToSessionDTOCreates(rawSession);
+
+			Assert.AreEqual(rawSession.TotalSession, sessionDTOCreates.Count);
+		}
+
+		[Test]
+		public void ConvertToSessionDTOCreates_TestFirstDateOfFirstWeek()
+		{
+			SessionDTORaw rawSession = new SessionDTORaw
+			{
+				SubjectId = "PRN211",
+				GroupId = "SE1643",
+				RoomRaw = "BE-102",
+				FirstDate = new DateTime(2024, 3, 24),
+				TimeslotRaw = "A24",
+				LecturerId = "chilp",
+				TotalSession = 20,
+				Online = true
+			};
+
+			var sessionDTOCreates = SessionHelper.ConvertToSessionDTOCreates(rawSession);
+
+			Assert.AreEqual(new DateTime(2024, 3, 25), sessionDTOCreates.FirstOrDefault().Date);
+		}
+
+		[Test]
+		public void ConvertToSessionDTOCreates_TestLastDateOfFirstWeek()
+		{
+			SessionDTORaw rawSession = new SessionDTORaw
+			{
+				SubjectId = "PRN211",
+				GroupId = "SE1643",
+				RoomRaw = "BE-102",
+				FirstDate = new DateTime(2024, 3, 24),
+				TimeslotRaw = "A24",
+				LecturerId = "chilp",
+				TotalSession = 20,
+				Online = true
+			};
+
+			var sessionDTOCreates = SessionHelper.ConvertToSessionDTOCreates(rawSession);
+
+			Assert.AreEqual(new DateTime(2024, 5, 29), sessionDTOCreates.LastOrDefault().Date);
+		}
+
+		[Test]
+		public void ConvertToSessionDTOCreates_TestSessionNoOfLastDate()
+		{
+			SessionDTORaw rawSession = new SessionDTORaw
+			{
+				SubjectId = "PRN211",
+				GroupId = "SE1643",
+				RoomRaw = "BE-102",
+				FirstDate = new DateTime(2024, 3, 24),
+				TimeslotRaw = "A24",
+				LecturerId = "chilp",
+				TotalSession = 20,
+				Online = true
+			};
+
+			var sessionDTOCreates = SessionHelper.ConvertToSessionDTOCreates(rawSession);
+
+			Assert.AreEqual(20, sessionDTOCreates.LastOrDefault().SessionNo);
+		}
+
+
+		[Test]
+		public void ConvertToSessionDTOCreates_TestTimeSlotIdOfFirstDate()
+		{
+			SessionDTORaw rawSession = new SessionDTORaw
+			{
+				SubjectId = "PRN211",
+				GroupId = "SE1643",
+				RoomRaw = "BE-102",
+				FirstDate = new DateTime(2024, 3, 24),
+				TimeslotRaw = "A24",
+				LecturerId = "chilp",
+				TotalSession = 20,
+				Online = true
+			};
+
+			var sessionDTOCreates = SessionHelper.ConvertToSessionDTOCreates(rawSession);
+
+			Assert.AreEqual(1, sessionDTOCreates.FirstOrDefault().TimeslotId);
+		}
+
+		[Test]
+		public void ConvertToSessionDTOCreates_TestTimeSlotIdOfLastDate()
+		{
+			SessionDTORaw rawSession = new SessionDTORaw
+			{
+				SubjectId = "PRN211",
+				GroupId = "SE1643",
+				RoomRaw = "BE-102",
+				FirstDate = new DateTime(2024, 3, 24),
+				TimeslotRaw = "A24",
+				LecturerId = "chilp",
+				TotalSession = 20,
+				Online = true
+			};
+
+			var sessionDTOCreates = SessionHelper.ConvertToSessionDTOCreates(rawSession);
+
+			Assert.AreEqual(2, sessionDTOCreates.LastOrDefault().TimeslotId);
+		}
+	}
+}
