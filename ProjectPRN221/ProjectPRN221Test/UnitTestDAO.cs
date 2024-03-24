2f6308043e007c18dd4a1b0eb4470a0a0ec79581
Nguyễn Lương Khang <96479575+Khangnekk@users.noreply.github.com>
2024-03-25 01:03:24 +0700

    Unit Test

---
+++ ProjectPRN221/ProjectPRN221Test/UnitTestDAO.cs
@@ -0,0 +1,14 @@
+﻿using Data_Access.DAO;
+
+namespace ProjectPRN221Test
+{
+	public class UnitTestDAO
+	{
+		[Test]
+		public void TestGetAreas()
+		{
+			var listAreas = AreaDAO.Instance.GetAreas();
+			Assert.AreEqual(5, listAreas.Count);
+		}
+	}
+}
